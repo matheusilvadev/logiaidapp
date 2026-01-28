921118839ceff00728f4bbe8a354176826a5b0cc
devma <eurotheus@gmail.com>
2026-01-28 11:04:23 -0300

    chore: Adiciona configuracoes de Keycloak

---
+++ src/main/java/com/astrocode/api/api/config/KeycloakJwtConfig.java
@@ -0,0 +1,40 @@
+package com.astrocode.api.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakJwtConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(this::extractRoles);
+
+        return converter;
+    }
+
+    private Collection<GrantedAuthority> extractRoles(Jwt jwt) {
+
+        Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+
+        if (realmAccess == null || !realmAccess.containsKey("roles")) {
+            return List.of();
+        }
+
+        List<String> roles = (List<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .map(role -> "ROLE_" + role.toUpperCase()) // BENEFICIARY -> ROLE_BENEFICIARY
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+}
